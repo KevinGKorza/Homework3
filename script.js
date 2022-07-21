33c38bee1d1e5a1aeacc6297a7d40a326c32eac8
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-07-21 13:27:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var generateBtn = document.querySelector("#generate");
+//An array containing uppercase letters A-Z
 var uppercaseCharset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//An array containing lowercase letters a-z
 var lowercaseCharset = ["a","b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numericalCharset = [0,1,2,3,4,5,6,7,8,9];
+//An array containing numbers 1-9
+var numericalCharset = ["0","1","2","3","4","5","6","7","8","9"];
+//An array containing special characters
 var specialCharset = ["!","@","#","$","%","^","&","*","?"];
+//Used for concanation of the above variables later on in the code
 var choices = [];
 
 function writePassword() { 
@@ -15,7 +20,8 @@ function writePassword() {
 }
 
 
-function generatePassword() {
+function generatePassword() { 
+  //Will tell user to add the value in their password, if they don't add it then they'll get an alert message saying to
   while (true) {
     var firstMessage = message("How long would you like your password to be? It has to be between 8 and 128 characters.");
     if (!firstMessage) {
@@ -27,6 +33,7 @@ function generatePassword() {
     }
   }
 
+  //Will ask user how they want their password 
   if (confirm("Do you want numbers in your password?")) {
     choices = choices.concat(numericalCharset);
   }
@@ -36,11 +43,14 @@ function generatePassword() {
   if (confirm("Do you want any lower case characters in your password?")) {
     choices = choices.concat(lowercaseCharset);
   }
+  //if nothing has been picked then the user will get this message 
   if (choices.length == 0) {
     alert("Unable to fullfil your request");
   }
 
+  //new password will go here 
   var password = []:
+  //this is the loop which is needed for the password generation 
   for (var i = 0; i < firstMessage; i++) {
     var allChoices = choices[Math.floor(Math.random() * choices.length)];
     randomPassword.push(allChoices);
